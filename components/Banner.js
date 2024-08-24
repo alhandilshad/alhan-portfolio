172b6cfb5ac822e86d379a7a47098eae8c97af49
alhandilshad <dilshadalhan@gmail.com>
2024-08-24 21:06:40 +0500

    update code

--- components/Banner.js
+++ components/Banner.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 const Banner = () => {
@@ -9,9 +10,11 @@ const Banner = () => {
             <h1 className="text-[32px] md:text-[40px] font-semibold">HELLO</h1>
             <h1 className="text-[50px] md:text-[70px] font-bold">I AM ALHAN</h1>
             <h1 className="text-[20px] md:text-[24px] font-semibold">FRONTEND DEVELOPER</h1>
+            <Link href="/Alhan-Dilshad (1).pdf" target="_blank" >
             <button className="text-white px-8 md:px-10 py-3 rounded-md font-semibold mt-6 duration-500 shadow-[0px_10px_30px_rgba(118,85,225,0.3)] bg-origin-border bg-clip-border border-2 border-transparent bg-[linear-gradient(to_right,#4458dc_0%,#854fee_100%),radial-gradient(circle_at_top_left,#4458dc,#854fee)] hover:bg-[linear-gradient(#ffffff,#ffffff),radial-gradient(circle_at_top_left,#4458dc,#854fee)] hover:border-2 hover:border-[#854fee] hover:text-[#222222] hover:shadow-none hover:bg-clip-border">
               HIRE ME
             </button>
+            </Link>
           </div>
           <div className="w-[80%] md:w-auto">
             <img src="/home-right.png" alt="Banner Image" className="max-w-full h-auto" />
